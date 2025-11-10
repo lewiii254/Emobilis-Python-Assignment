05948ed8b2b7b26645bd35dab4367dc5a1166725
MARKLEWIS MUTUGI <164312183+lewiii254@users.noreply.github.com>
2025-11-10 10:09:59 +0300

    Age access program py

---
+++ age.py
@@ -0,0 +1,11 @@
+# Age Access Program
+age = int(input("Enter your age: "))
+
+if age > 18:
+    print("Access granted — you receive a complimentary drink!")
+elif 16 <= age <= 18:
+    print("Access granted — enjoy a juice pack!")
+else:
+    print("Access denied — you’re too young!")
+    
+    
